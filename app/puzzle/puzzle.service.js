dc92ed9312e93acbfa6e2d6cc66481e37587ac7a
Chris Atkin <atkinchris@gmail.com>
2015-07-30 23:22:31 +0100

    Fixed replacing of unknown letters, where only the first instance was replaced.

--- app/puzzle/puzzle.service.js
+++ app/puzzle/puzzle.service.js
@@ -34,27 +34,50 @@
 
         function _generate() {
             _word = 'Sainsburys'.toUpperCase();
+            _puzzle.letters = _word;
 
-            var unknownLetters = [];
-
-            var i;
-            for (i = 0; i < _word.length; i++) {
-                if (!isInWord(_word[i], unknownLetters) && _randomBool()) {
-                    unknownLetters.push(_word[i]);
+            for (var i = 0; i < _word.length; i++) {
+                var letter = _word.charAt(i);
+                if (!isInWord(letter, _puzzle.rack) && _randomBool()) {
+                    _puzzle.rack.push(letter);
+                    _puzzle.letters = _replaceAll(_puzzle.letters, letter, '?');
                 }
             }
 
-            for (i = 0; i < _word.length; i++) {
-                if (isInWord(_word[i], unknownLetters)) {
-                    _puzzle.letters[i] = '?';
-                } else {
-                    _puzzle.letters[i] = _word[i];
-                }
+            var rackSpace = 12 - _puzzle.rack.length;
+
+            for (var j = 0; j < rackSpace; j++) {
+                _puzzle.rack.push(_randomAlpha(_puzzle.rack));
             }
+
+            _shuffle(_puzzle.rack);
         }
 
         function _randomBool() {
-            return Math.random() >= 0.4;
+            return Math.random() >= 0.5;
+        }
+
+        function _randomAlpha(excluding) {
+            var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            excluding = excluding || [];
+            for (var i = 0; i < excluding.length; i++) {
+                possible = possible.replace(excluding[i], '');
+            }
+            return possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+
+        function _shuffle(array) {
+            array.sort(function() {
+                return 0.5 - Math.random();
+            });
+        }
+
+        function _replaceAll(string, find, replace) {
+            function escapeRegExp(string) {
+                return string.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, '\\$1');
+            }
+
+            return string.replace(new RegExp(escapeRegExp(find), 'g'), replace);
         }
     }
 })();
